59b1287c5e357fbe52c27a0fd7b33c1c68e38e3e
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-23 17:16:10 +0530

    react

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,16 @@
 import { useForm } from "react-hook-form";
 import FormInput from "../components/forms/FormInput";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const formSchema = yup.object({
+    title: yup.string().min(3).max(20),
+    email: yup.string().email().required(),
+}).required();
 
 const ContactPage = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(formSchema) });
 
     const sendInfo = (data) => {
         console.log(data);
@@ -16,9 +23,9 @@ const ContactPage = () => {
 
                 <form action="" className="my-5  space-y-4" onSubmit={handleSubmit(sendInfo)}>
 
-                    <FormInput name="title" label="Title" placeholder="Enter your subject" required register={register("title", { required: "This feild is requried" })} error={errors.title} />
+                    <FormInput name="title" label="Title" placeholder="Enter your subject" required register={register("title")} error={errors.title} />
 
-                    <FormInput name="email" label="Email Address" placeholder="Enter your email" required register={register("email", { required: "This feild is requried" })} error={errors.email} />
+                    <FormInput name="email" label="Email Address" placeholder="Enter your email" required register={register("email")} error={errors.email} />
 
                     <button className="px-4 py-2 rounded bg-yellow-500 font-semibold">Submit</button>
                 </form>
